01888733f7a41461c2fbe934a8a95d74b88550c9
blanker <you@example.com>
2023-03-09 14:07:41 +0100

    S

--- include/imm_controller/imm_controller.hpp
+++ include/imm_controller/imm_controller.hpp
@@ -201,7 +201,7 @@ protected:
 Eigen::Matrix<double, 9, 6> pseudoInverse(const Eigen::Matrix<double, 6, 9> &a, double epsilon = std::numeric_limits<double>::epsilon())
 {
 
-	Eigen::JacobiSVD< Eigen::Matrix<double, 6, 9> > svd(a ,Eigen::ComputeThinU | Eigen::ComputeThinV);
+	Eigen::JacobiSVD< Eigen::Matrix<double, 6, 9> > svd(a ,Eigen::ComputeFullU | Eigen::ComputeFullV);
 	double tolerance = epsilon * std::max(a.cols(), a.rows()) *svd.singularValues().array().abs()(0);
 	return svd.matrixV() *  (svd.singularValues().array().abs() > tolerance).select(svd.singularValues().array().inverse(), 0).matrix().asDiagonal() * svd.matrixU().adjoint();
 }
