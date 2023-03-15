941c293781bc56f48ccda2f0a1ae153b9ce7fea9
blanker <you@example.com>
2023-03-15 19:09:04 +0100

    s

--- src/imm_controller.cpp
+++ src/imm_controller.cpp
@@ -336,7 +336,7 @@ controller_interface::return_type ImmController::update(
     // _q_robot_vel =  _J_robot.data.inverse() * (_base_vel + 0.1 * error_cart);
     Eigen::Matrix< double, 6, 6> KK = Eigen::Matrix< double, 6, 6>::Zero();
     KK.diagonal() << 0.1, 0.1, 0.1, 0.1 , 0.1 , 0.1;
-    _q_robot_vel =  _J_robot.data.inverse() * (_base_vel + KK * error_cart);
+    _q_robot_vel =  _J_robot.data.inverse() * (_base_vel + 0.3 * error_cart);
 
     for (auto index = 0UL; index < command_interfaces_.size(); ++index)
     {
